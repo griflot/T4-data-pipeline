8f8a117d00067bf99db46fdf88f6ff2d59949fea
Griffin Perlot <gperlot9@gmail.com>
2024-10-03 13:06:45 -0500

    Fix consumer output

--- scripts/kafka-consumer.py
+++ scripts/kafka-consumer.py
@@ -2,6 +2,6 @@ from kafka import KafkaConsumer
 
 consumer = KafkaConsumer('student-grades', bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
 
-print("Consuming and printing items from Kafka topic \"student-grades\". Hit Ctrl+C to stop...\n")
+print("Consuming and printing items from Kafka topic \"student-grades\". Hit Ctrl+C to stop...")
 for message in consumer:
     print(f"Received \"{message.value}\" from Kafka broker at localhost:9092")
