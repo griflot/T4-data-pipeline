0068de28d1879d5eba01db55b1ff503021153323
Griffin Perlot <gperlot9@gmail.com>
2024-10-03 20:12:57 -0500

    Fix infinite loop causing crash in Mage generator

--- mage/default_repo/data_loaders/generator.py
+++ mage/default_repo/data_loaders/generator.py
@@ -1,9 +1,19 @@
 from mage_ai.streaming.sources.base_python import BasePythonSource
 from typing import Callable
+import random
+import time
 
 if 'streaming_source' not in globals():
     from mage_ai.data_preparation.decorators import streaming_source
 
+possible_names = ['Scarlett', 'Melody', 'Bennett', 'Drew', 'Olivia', 'Mariah', 'Jose', 'Levi', 'Nikhil', 'Quinn']
+possible_grades = ['A','B','C','D','E','F']
+
+# NOTE: edit interval_seconds to change how often (in seconds) records are generated
+interval_seconds = 30
+
+# NOTE: edit batch_size to change how many records are generated per time interval
+batch_size = 3
 
 @streaming_source
 class CustomSource(BasePythonSource):
@@ -16,15 +26,21 @@ class CustomSource(BasePythonSource):
         """
         Batch read the messages from the source and use handler to process the messages.
         """
-
-        test_message1 = {'data': {'name':'Stacy', 'grade':'A'}}
-        test_message2 = {'data': {'name':'Bob', 'grade':'F'}}
-        test_done = False
-        
+        num_names = len(possible_names)
+        num_grades = len(possible_grades)
         while True:
             records = []
             # Implement the logic of fetching the records
-            if not test_done:
-                records = [test_message1, test_message2]
+
+            # Generate random name/grade records
+            for i in range(batch_size):
+                name = possible_names[random.randrange(0,num_names)]
+                grade = possible_grades[random.randrange(0,num_grades)]
+                new_record = {"data": {"name": name, "grade": grade}}
+                records.append(new_record)
+
             if len(records) > 0:
                 handler(records)
+            
+            # Sleep in between every set of records generated and sent
+            time.sleep(interval_seconds)
